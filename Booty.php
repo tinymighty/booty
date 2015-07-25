bc8bd06e50aa213905cd003c574759c1f7f23cf5
paladox <paladox@users.noreply.github.com>
2015-07-25 14:13:21 +0100

    Update Booty.php

--- Booty.php
+++ Booty.php
@@ -8,20 +8,21 @@
  * @License: GPLv2 (http://www.gnu.org/copyleft/gpl.html)
  */
 
-
 $GLOBALS['wgExtensionCredits']['parserhook'][] = array(
 	'name' => 'Booty',
+	'namemsg' => 'skinname-booty'
 	'author' => 'Andru Vallance',
-	'description' => 'Booty is a modern mobile-first responsive MediaWiki theme built on Bootstrap 3. Designed primarily for ease of customisation to create new skins, but with an initial set of clean templates which can be used as-is.  Using [[Extension:Skinny]] allows defining skin variations and options on a per-page basis for awesome skin customisation.',
+	'descriptionmsg' => 'booty-desc'
 	'url' => 'https://github.com/andru/booty'
 );
+
 $cd = dirname(__FILE__);
 
 $GLOBALS['wgAutoloadClasses']['Booty'] = $cd . '/Booty.class.php';
 $GLOBALS['wgAutoloadClasses']['SkinBooty'] = $cd . '/base/Skin.php';
 $GLOBALS['wgAutoloadClasses']['BootyTemplate'] = $cd . '/base/Template.php';
 
-$GLOBALS['wgExtensionMessagesFiles'][ 'Booty' ] = $cd . '/Booty.i18n.php';
+$GLOBALS['wgMessagesDirs']['Booty'] = __DIR__ . '/i18n';
 
 $GLOBALS['wgValidSkinNames']['booty'] = 'Booty';
 
